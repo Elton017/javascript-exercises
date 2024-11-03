5bccf11f304465e4ba2f47c69daf4cefe0f5aaf5
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-04 08:30:27 +1030

    completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(myString, myNum) {
+    if (myNum == 0) {
+        return ''
+    }
+    else if (myNum < 0) {
+        return 'ERROR'
+    }
+    else {
+        let finalString = '';
+        for (let i = 0; i < myNum; i++) {
+            finalString += myString
+        }
+        return finalString
+    }
 };
 
 // Do not edit below this line
