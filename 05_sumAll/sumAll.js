3ca4f66c85ea3ee28b775a028373e3c95f61013e
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-04 12:21:47 +1030

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+    if (num1 % 1 != 0 || num2 % 1 != 0) {
+        return "ERROR"
+    }
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        let sum = 0;
+        if (num1 < num2) {
+            for (let i = num1; i < num2 + 1; i++) {
+                sum += i;
+            }
+        return sum
+        }
+        else if (num1 > num2) {
+            for (let i = num2; i < num1 + 1; i++) {
+                sum += i;
+            }
+        return sum
+        }
+    }    
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
