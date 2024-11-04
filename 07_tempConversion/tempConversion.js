cb4409ea8684866eb96fc4b26c33b6dff4379974
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-04 12:35:29 +1030

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const round = function(num, places) {
+  if (num % 1 == 0) {
+    return Math.round(num)
+  }
+  return parseFloat(num.toFixed(places));
+}
+
+
+const convertToCelsius = function(temp) {
+ return round(((temp - 32) * 5 / 9), 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return round((temp * 9 / 5 + 32), 1)
 };
 
 // Do not edit below this line
