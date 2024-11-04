712a83a7b22758b38b12006b7e50d4db80da1c85
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-04 11:54:31 +1030

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    args.forEach(arg => {
+        for (let i = 0; i < array.length; i++) {
+            if (arg === array[i]) {
+                array.splice(i, 1)
+                i--
+            }
+        }
+    })
+    
+    return array;
 };
 
 // Do not edit below this line
