8f0bb4573bfedf820b2890fe951c1cc793972eea
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-13 07:55:51 +1030

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let firstnum = 0;
+    let secondnum = 1;
 
+    if (number == 0) {return 0}
+    if (number < 0) {return "OOPS"}
+
+    for (let i=1; i<parseInt(number); i++){
+        let tempnum = secondnum;
+        secondnum += firstnum;
+        firstnum = tempnum;
+    }
+    return secondnum;
 };
 
 // Do not edit below this line
