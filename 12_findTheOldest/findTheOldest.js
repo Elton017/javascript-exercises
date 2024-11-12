e0bdf8b6696f64005743aadc8a4fd1a83f214356
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-13 10:10:50 +1030

    a

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((a,b) => {
+        const age1 = getAge(a.yearOfBirth, a.yearOfDeath);
+        const age2 = getAge(b.yearOfBirth, b.yearOfDeath);
+        return age1 > age2 ? a : b;
+    })
+};
 
+const getAge = function(birth, death){
+    if (!death) {
+        death = new Date().getFullYear()
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
