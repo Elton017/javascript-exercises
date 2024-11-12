d90653feec8c2f09c1a9c952d05f2c3fa26fea84
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-13 07:34:21 +1030

    used regex to filter, didnt know you could just type all chars into a string and use that lol

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (myString) {
+    const cleanedString = myString.toLowerCase().split("").filter(char => char.match(/[a-z0-9]/)).join("");
+    const reversed = cleanedString.split("").reverse().join("");
 
+    return cleanedString == reversed;
 };
 
 // Do not edit below this line
